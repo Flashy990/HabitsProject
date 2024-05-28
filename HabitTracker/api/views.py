834e1ae8560702583eec4ddfeff5cd8d37cb6d9a
akakadia <akakadia@terpmail.umd.edu>
2024-05-27 21:46:18 -0400

    implemented initial backend functionality to store the added habit in a backend database

--- HabitTracker/api/views.py
+++ HabitTracker/api/views.py
@@ -5,10 +5,20 @@ from rest_framework import viewsets
 from .models import Habit
 from .serializers import HabitSerializer
 
+@api_view(['POST'])
+def habit_create(request):
+    serializer = HabitSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
 @api_view(['GET'])
 def hello_world(request):
     return Response({'message': 'Hello, world!'})
 
-class HabitViewSet(viewsets.ModelViewSet):
-    queryset = Habit.objects.all()
-    serializer_class = HabitSerializer
+# class HabitViewSet(viewsets.ModelViewSet):
+#     queryset = Habit.objects.all()
+#     serializer_class = HabitSerializer
--- react_frontend/src/AddHabits.js
+++ react_frontend/src/AddHabits.js
@@ -4,10 +4,24 @@ const Create = () => {
   const [goal, setGoal] = useState('');
   const [goalDescription, setGoalDescription] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const habit = { goal, goalDescription };
+
+    fetch('http://localhost:8000/api/habits/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(habit)
+    }).then(() => {
+      console.log('New habit added');
+    })
+  }
+
   return (
     <div className="create">
       <h2>Add a New Habit</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Habit:</label>
         <input 
           type="text" 
