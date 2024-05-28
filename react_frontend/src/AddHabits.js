35edef0d7c80f4f43c9b81a028f3c2974afb6022
akakadia <akakadia@terpmail.umd.edu>
2024-05-27 20:08:07 -0400

    Changed css

--- react_frontend/src/AddHabits.js
+++ react_frontend/src/AddHabits.js
@@ -1,9 +1,8 @@
 import { useState } from "react";
 
 const Create = () => {
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-//   const [author, setAuthor] = useState('mario');
+  const [goal, setGoal] = useState('');
+  const [goalDescription, setGoalDescription] = useState('');
 
   return (
     <div className="create">
@@ -13,23 +12,15 @@ const Create = () => {
         <input 
           type="text" 
           required 
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          value={goal}
+          onChange={(e) => setGoal(e.target.value)}
         />
         <label>Description:</label>
         <textarea
           required
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
+          value={goalDescription}
+          onChange={(e) => setGoalDescription(e.target.value)}
         ></textarea>
-        {/* <label>Blog author:</label>
-        <select
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-        >
-          <option value="mario">mario</option>
-          <option value="yoshi">yoshi</option>
-        </select> */}
         <button>Add it</button>
       </form>
     </div>
